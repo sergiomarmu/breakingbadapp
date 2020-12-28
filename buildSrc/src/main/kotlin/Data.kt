6aae063beb7b8aaf5173e2b04cce23108512586d
Sergio Martinez Mulero <sergiomarmu@gmail.com>
2020-12-28 10:30:23 +0100

    Changed modules gradle extension to kts

---
+++ buildSrc/src/main/kotlin/Data.kt
@@ -0,0 +1,10 @@
+object Data {
+    // region kotlin
+    const val kotlinVersion = "1.3.72"
+    // endregion kotlin
+
+    // region test
+    const val jUnitVersion = "4.13.1"
+    const val androidxJunitVersion = "1.1.2"
+    // endregion test
+}
---
+++ core/build.gradle.kts
@@ -0,0 +1,43 @@
+import Core.androidxJunitVersion
+import Core.jUnitVersion
+import Core.kotlinVersion
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+}
+
+android {
+    compileSdkVersion(29)
+
+    defaultConfig {
+        minSdkVersion(27)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    // region kotlin
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+    // endregion kotlin
+
+    // region test
+    testImplementation("junit:junit:$jUnitVersion")
+    androidTestImplementation("androidx.test.ext:junit:$androidxJunitVersion")
+    // endregion test
+}
